6be4a2a322bbaca698f4f8006c94985064cabe1c
Zadok-Little <zzlittle@students.everettcc.edu>
2025-05-14 23:50:20 +0000

    Created "counting_functions" file
    In counting_functions:
        Created 3 functions to count successful missions, calculates success
        rates of provided missions, and prints the missions before the year
        2000
    
    In main:
        simply added notes to keep track of what to do

---
+++ counting_functions.py
@@ -0,0 +1,28 @@
+# count number of successful missions
+def successful_missions(mission_success):
+
+    counter = 0
+
+    for x in mission_success:
+        if x:
+            counter += 1
+    
+    return counter
+
+# calculate success rate
+def success_rate(mission_success):
+
+    rate = float((successful_missions(mission_success) / len(mission_success)) * 100)
+
+    return rate
+
+# print missions before 2000
+def pre2000_missions(mission_names, mission_years):
+
+    print("Missions launched before the year 2000:")
+
+    for x in range(len(mission_names)):
+
+        if mission_years[x] < 2000:
+
+            print(f"- {mission_names[x]}")
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from counting_functions import *
+
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
+
+# Counts the total number of space missions.
+# Counts the number of successful missions.
+# Calculates the success rate of the missions.
+# Lists all the missions that were launched before the year 2000.
